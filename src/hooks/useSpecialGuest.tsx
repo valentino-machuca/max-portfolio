040069a981c73ec5619034108cd5a7f75c5ef1b0
Valentino Machuca <valentinojesuss4@gmail.com>
2026-02-11 18:23:20 -0300

    Ajusta render para mantener flores históricas crecidas

--- src/hooks/useSpecialGuest.tsx
+++ src/hooks/useSpecialGuest.tsx
@@ -1,10 +1,27 @@
+import { useMemo } from 'react';
 
-export const useSpecialGuest = () => {
-  const today = new Date();
-  
-  const isValentine = today.getMonth() === 1 && today.getDate() === 14; 
-  
-  if (isValentine) return Math.random() > 0.5 ? 'bee' : 'cat';
-  
-  return null;
+type GuestType = 'bee' | 'cat' | null;
+
+const getMonthDiff = (from: Date, to: Date) => {
+  const years = to.getFullYear() - from.getFullYear();
+  const months = to.getMonth() - from.getMonth();
+  return years * 12 + months;
+};
+
+export const useSpecialGuest = (startDate: Date): GuestType => {
+  return useMemo(() => {
+    const today = new Date();
+
+    const isValentine = today.getMonth() === 1 && today.getDate() === 14;
+    if (isValentine) {
+      return 'bee';
+    }
+
+    const monthDiff = getMonthDiff(startDate, today);
+    const isMonthiversary = monthDiff >= 0 && today.getDate() === startDate.getDate();
+
+    if (!isMonthiversary) return null;
+
+    return monthDiff % 2 === 0 ? 'cat' : 'bee';
+  }, [startDate]);
 };
