fc9257f9300711bd0d6aa693d5c7f737391962ca
Vale Machuca <valentinomachuca.dev@gmail.com>
2026-02-12 21:22:31 -0300

    rotate

--- src/components/pets/cat/Cat.tsx
+++ src/components/pets/cat/Cat.tsx
@@ -4,6 +4,11 @@ import cat from '../../../assets/cat.json';
 import styles from './Cat.module.scss';
 
 const Cat: React.FC = () => {
+  const [rotation, setRotation] = React.useState(0);
+
+  const handleAnimationComplete = () => {
+    setRotation((prevRotation) => prevRotation + 180);
+  };
 
   return (
     <div className={styles.catContainer}>
@@ -12,6 +17,8 @@ const Cat: React.FC = () => {
         loop={true}
         autoplay={true}
         className={styles.catVisual}
+        onLoopComplete={handleAnimationComplete}
+        style={{ transform: `rotate(${rotation}deg)` }}
       />
     </div>
   );
